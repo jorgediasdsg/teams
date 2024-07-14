2ccc5c46783b23a31d600527fdcd7b2011fe4533
Jorge Dias <jorgediasdsg@gmail.com>
2024-07-14 08:58:22 -0300

    ADD: Path

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Groups } from './src/screen/Groups';
-
+import { Groups } from '@screens/Groups';
+  
 export default function App() {
   return (
     <Groups />
